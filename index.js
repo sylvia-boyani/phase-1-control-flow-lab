1d61d2455af772865ca1d34bd13bb68a6124fb13
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-09 09:34:27 +0300

    commit tree 8a2b5fe8574e198fa16278c8888bf6e7ff9453ad

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  
+  if (distance <= 400){
+    result = "This one is on me!";
+  } 
+  else if (distance > 2000 && distance < 2500){
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (distance > 2500){
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result;
+   city === "NYC" ? (result = "Ok, sounds good.") : (result = "No go.");
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// function switchOnCharmFromTip(tip){
+//   // Write your code here!
+//   let result;
+//   switch (switchOnCharmFromTip){
+//     case "tip generous":
+//       result = "Thank you so much."
+//       break;
+//       case "tip not so generous":
+//       result = "Thank you.";
+//       break;
+//       default:
+//         result = "Bye.";
+//         break;
+//   }
+// }
+
+function switchOnCharmFromTip(tip){
+if (tip === "generous") {
+  return "Thank you so much."
+} else if (tip === "not as generous") {
+  return "Thank you."
+} else {
+  return "Bye."
+}
 }
